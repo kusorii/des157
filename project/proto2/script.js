3d9a1c87a894e4834c8ce92a9b47380369912e25
Tammy Luu <taluu@ucdavis.edu>
2018-03-08 10:28:20 -0800

    Reworked prototype 2
    Fixed positioning on prototype 2 and added more content.

--- project/proto2/script.js
+++ project/proto2/script.js
@@ -18,21 +18,21 @@ var text = document.getElementById("text");
 var article = document.querySelector("article");
 var click = 0;
 
+
 // document.getElementById("text").addEventListener("click", changeText);
 
 function changeText() {
-  click+=1;
-  if(click==1){
+  click += 1;
+  if (click == 1) {
     document.getElementById("text").innerHTML = "Hey sweetie, it's time to get up. Didn't you say you had a test today in your first class?<br>You don't want to be late for that now. Get up!";
-    click=+1;
-  }
-  else if(click=="2"){
+    click += 1;
+  } else if (click == "2") {
     document.getElementById("text").innerHTML = "...Oh crap! I forgot all about that. I'm so sleepy though... Maybe I should stay in bed<br> some more. Just a few more minutes...";
-  }
-  else if(click=="3"){
+  } else if (click == "3") {
     document.getElementById("text").innerHTML = "**Options are currently under construction and will uavailable until further notice**";
   }
 }
+
 
 
 var fullS = document.querySelector("#fullScreen");
@@ -73,9 +73,9 @@ document.addEventListener('MSFullscreenChange', exitHandler);
 
 function exitHandler() {
   console.log('exitHandler');
-    if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
+  if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
 
-       fullS.style.display = 'inline';
-       article.style.height = '320px';
-    }
+    fullS.style.display = 'inline';
+    article.style.height = '320px';
+  }
 }
